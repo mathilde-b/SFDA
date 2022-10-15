8930ead88d712ea80e43b8ed97aed2be9dcea649
Mathilde Bateson <mathildebateson@gmail.com>
2022-10-15 06:16:34 -0400

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -364,6 +364,21 @@ class RandomSampler(Sampler):
     def __len__(self):
         return self.num_samples
 
+class Sampler(object):
+    r"""Base class for all Samplers.
+    Every Sampler subclass has to provide an __iter__ method, providing a way
+    to iterate over indices of dataset elements, and a __len__ method that
+    returns the length of the returned iterators.
+    """
+
+    def __init__(self, data_source):
+        pass
+
+    def __iter__(self):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
 
 
 class ConcatDataset(Dataset):
